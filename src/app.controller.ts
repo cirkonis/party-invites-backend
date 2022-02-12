14d9a3f605a982d76868aa18622f091b75404bc0
mmarigliano <michael@sharkcell.dk>
2022-02-12 19:00:22 +0100

    make it better-3

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,14 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/favicon')
+  getFavicon(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/')
+  getBase(): string {
+    return this.appService.getHello();
+  }
 }
